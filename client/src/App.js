825cd8876e91afd8bbcbe6d08fb51340b7c6a8de
Hvdodeptrai <hvdo21@clc.fitus.edu.vn>
2023-11-18 10:22:43 +0700

    Dashboard Upload

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import HomePage from "./pages/HomePage";
 import SignInPage from "./pages/SignIn";
 import SignUpPage from "./pages/SignUp";
 import { Route, Routes } from "react-router-dom";
+import DashboardPage from "pages/DashboardPage";
+import PostManage from "module/post/PostManage";
+import PostAddNew from "module/post/PostAddNew";
+import UserProfile from "module/user/UserProfile";
 
 function App() {
   return (
@@ -11,6 +15,25 @@ function App() {
         <Route path="/" element={<HomePage></HomePage>}></Route>
         <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
         <Route path="/sign-in" element={<SignInPage></SignInPage>}></Route>
+        <Route path="/sign-in" element={<SignInPage></SignInPage>}></Route>
+        <Route element={<DashboardLayout></DashboardLayout>}>
+          <Route
+            path="/dashboard"
+            element={<DashboardPage></DashboardPage>}
+          ></Route>
+          <Route
+            path="/manage/posts"
+            element={<PostManage></PostManage>}
+          ></Route>
+          <Route
+            path="/manage/add-post"
+            element={<PostAddNew></PostAddNew>}
+          ></Route>
+          <Route
+            path="/manage/update-user"
+            element={<UserProfile></UserProfile>}
+          ></Route>
+        </Route>
       </Routes>
     </div>
   );
