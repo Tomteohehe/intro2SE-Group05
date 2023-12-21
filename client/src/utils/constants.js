0325e51784d58813a794457b8d86f312b38d0f04
Hvdodeptrai <hvdo21@clc.fitus.edu.vn>
2023-12-21 14:45:31 +0700

    Filter by Category and Date

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const LOCAL_STORAGE_TOKEN_NAME = 'GoaTalks'
+export const LOCAL_STORAGE_TOKEN_NAME = "GoaTalks";
 
 export const theme = {
   primary: "#1DC071",
@@ -16,13 +16,46 @@ export const theme = {
   black: "#171725",
 };
 
-export const postStatus = {
-  APPROVED: 1,
-  PENDING: 2,
-  REJECTED: 3,
-};
-
-export const categoryStatus = {
-  APPROVED: 1,
-  UNAPPROVED: 2,
-};
+export const categories = [
+  { id: 0, name: "All Categories" },
+  {
+    id: 1,
+    name: "Lifestyle",
+  },
+  {
+    id: 2,
+    name: "Fitness",
+  },
+  {
+    id: 3,
+    name: "Knowledge",
+  },
+  {
+    id: 4,
+    name: "Culture",
+  },
+  {
+    id: 5,
+    name: "Religion",
+  },
+  {
+    id: 6,
+    name: "Health",
+  },
+  {
+    id: 7,
+    name: "Food and Cooking",
+  },
+  {
+    id: 8,
+    name: "Personal Finance",
+  },
+  {
+    id: 9,
+    name: "Travel",
+  },
+  {
+    id: 10,
+    name: "Science",
+  },
+];
